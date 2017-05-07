0a77ad050302467cfd64f242c6ceba7c13941a13
Vzaa <Vzaa@users.noreply.github.com>
2017-05-07 15:39:06 +0300

    replace getrandom with syscall

--- plugins/random.c
+++ plugins/random.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <sys/random.h>
+
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <unistd.h>
+#include <syscall.h>
+#include <linux/random.h>
 
 #include "include/common.h"
 #include "include/plugin.h"
@@ -17,7 +21,7 @@ static void destroy(plugin_t* self)
 
 static void callback(plugin_t* self, char* buf, uint16_t* buf_len)
 {
-    getrandom(buf, *buf_len, 0);
+    syscall(SYS_getrandom, buf, *buf_len, 0);
 }
 
 plugin_t random_plugin = {
